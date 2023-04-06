0fc6ce36e065c064af035c08a687b93b9f3e5ea9
batufurkan <batufurkan552@gmail.com>
2023-04-07 01:38:14 +0300

    yeni unity sayfası kodu eklendi

---
+++ lib/unity.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
